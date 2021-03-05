6e9cd7b94481d44a4548fc30ae08188facaed24a
Kattia Salazar Mesen <kattiasm@gmail.com>
2021-03-05 23:42:48 +0000

    entregable Traffic Light

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,45 @@
 import React from "react";
 
+export default class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			clickedLight: null
+		};
+	}
+	render() {
+		console.log(this.state);
+		let redExtraClass = "";
+		if (this.state.clickedLight == "red") redExtraClass = "selected";
+		let yellowExtraClass = "";
+		if (this.state.clickedLight == "yellow") yellowExtraClass = "selected";
+		let greenExtraClass = "";
+		if (this.state.clickedLight == "green") greenExtraClass = "selected";
+
+		return (
+			<div>
+				<div id="trafficcLight"></div>
+				<div id="container">
+					<div
+						className={"red light " + redExtraClass}
+						onClick={() =>
+							this.setState({ clickedLight: "red" })
+						}></div>
+					<div
+						className={"yellow light " + yellowExtraClass}
+						onClick={() =>
+							this.setState({ clickedLight: "yellow" })
+						}></div>
+					<div
+						className={"green light " + greenExtraClass}
+						onClick={() =>
+							this.setState({ clickedLight: "green" })
+						}></div>
+				</div>
+			</div>
+		);
+	}
+}
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-}
